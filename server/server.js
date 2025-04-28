01cd9dbb2d878ea0650b7c4ba73eb9bc1a93b331
Olamilekan-hub <adebukolaolamilekan123@gmail.com>
2025-04-28 05:26:25 +0100

    first commit

--- server/server.js
+++ server/server.js
@@ -55,7 +55,12 @@ const upload = multer({
 });
 
 // Middleware
-app.use(cors());
+// Set CORS to accept requests from any origin
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
